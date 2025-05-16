51fc2df97c048be67775c9eb781c37094d981966
Oveshek  Kundu Toton <oveshaktoton9999@gmail.com>
2025-05-17 02:48:54 +0600

    Update vite.config.js

--- clint/vite.config.js
+++ clint/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+import path from 'path';
+
 export default defineConfig({
-  plugins: [react()],
-
-})
+  plugins: [react(),],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    historyApiFallback: true, // 👈 for dev server
+  },
+});
